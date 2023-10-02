bf44ab15f5050cfc92137fa7b5c850c388e3fd58
Rebbekka <146584352+Rebbekka@users.noreply.github.com>
2023-10-02 10:02:11 -0700

    Create RetailerController.java

---
+++ src/main/java/com/controllers/RetailerController.java
@@ -0,0 +1,43 @@
+package com.controllers;
+
+import com.model.response.RewardsPointsResponse;
+import com.service.RewardsServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@RestController
+@EnableWebMvc
+@RequestMapping(value = "/retailer")
+public class RetailerController {
+
+  @RequestMapping(
+    value = "/status",
+    method = RequestMethod.GET,
+    produces = {"text/html"}
+    )
+  @ResponseBody
+  public ResponseEntity<String> getStatus() {
+    return ResponseEntity.ok("OK!");
+  }
+
+  /**
+   *
+   *  run the API below like this e.g.  http://localhost:8080/retailer/report/4
+   *      I have initialized data for customer IDs 1 through 4
+   *      ID 4 has transactions over the last 3 months to calculate as the test requested
+   *      the report is returned in JSON format
+   */
+  @RequestMapping(
+          value = "/report/{id}",
+          method = RequestMethod.GET,
+          produces = {"application/json"}
+  )
+  @ResponseBody
+  public ResponseEntity<RewardsPointsResponse> getPoints(@PathVariable String id) {
+    System.out.println("getting report for customer ID: " + id);
+    RewardsServiceImpl rewardsService = new RewardsServiceImpl();
+    RewardsPointsResponse rewardsPoints = rewardsService.getRewardsPoints(id);
+    return ResponseEntity.ok(rewardsPoints);
+  }
+}
